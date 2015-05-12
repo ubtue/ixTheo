5ae6de6cb72acae8dd3435360777c8511cc1646f
root <root@ptah.ub.uni-tuebingen.de>
2015-05-12 17:57:53 +0200

    Bugfixes of bib_ref_to_codes_tool

--- module/ixTheo/src/ixTheo/Search/Backend/Solr/IxTheoQueryBuilder.php
+++ module/ixTheo/src/ixTheo/Search/Backend/Solr/IxTheoQueryBuilder.php
@@ -5,7 +5,7 @@ namespace ixTheo\Search\Backend\Solr;
 use VuFindSearch\Backend\Solr\QueryBuilder;
 use VuFindSearch\Query\AbstractQuery;
 
-define("_BIB_REF_MAPS_PATH_", '/var/lib/tuelib/');
+define("_BIB_REF_MAPS_PATH_", '/var/lib/tuelib/bibleRef/'); // Must end with backslash!
 define("_BIB_REF_CMD_PARAMS_", implode(' ', [_BIB_REF_MAPS_PATH_ . 'books_of_the_bible_to_code.map', _BIB_REF_MAPS_PATH_ . 'books_of_the_bible_to_canonical_form.map',  _BIB_REF_MAPS_PATH_ . 'pericopes_to_codes.map']));
 
 class IxTheoQueryBuilder extends QueryBuilder
