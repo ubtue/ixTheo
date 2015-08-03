c4d7f7842b9a2e24556c1ef5a3f209a125dc24d2
Oliver Obenland <oliver.obenland@uni-tuebingen.de>
2015-08-03 18:44:24 +0200

    Basic controller for keywordchains added

--- module/ixTheo/config/module.config.php
+++ module/ixTheo/config/module.config.php
@@ -1,12 +1,27 @@
 <?php
-namespace ixTheo\Module\Configuration;
 
-$config = [
-	'ixTheo' => [
-		 'search_backend' => [
-		 		  'Solr' => 'ixTheo\Search\Factory\SolrDefaultBackendFactory'
-		 ]
-	]
-];
+$config = array (
+  'ixTheo' => 
+  array (
+    'search_backend' => 
+    array (
+      'Solr' => 'ixTheo\\Search\\Factory\\SolrDefaultBackendFactory',
+    ),
+  ),
+  'controllers' => 
+  array (
+    'invokables' => 
+    array (
+      'Keywordchains' => 'ixTheo\\Controller\\KeyWordChainsController',
+    ),
+  ),
+);
+
+$staticRoutes = array (
+    'Keywordchains/Home'
+);
+
+$routeGenerator = new \VuFind\Route\RouteGenerator();
+$routeGenerator->addStaticRoutes($config, $staticRoutes);
 
 return $config;
