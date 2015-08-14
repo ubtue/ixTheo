752de7e852b743b391149c7bc2eabfcc21ab1534
Oliver Obenland <oliver.obenland@uni-tuebingen.de>
2015-08-14 16:18:30 +0200

    Added a parameter to solr-admin-url

--- module/ixTheo/src/ixTheo/Search/Backend/Solr/IxTheoQueryBuilder.php
+++ module/ixTheo/src/ixTheo/Search/Backend/Solr/IxTheoQueryBuilder.php
@@ -10,7 +10,7 @@ define("_BIB_REF_CMD_PARAMS_", implode(' ', [_BIB_REF_MAPS_PATH_ . 'books_of_the
 
 class IxTheoQueryBuilder extends QueryBuilder
 {
-    const BIBLE_REFERENCE_COMMAND = '/bin/bib_ref_to_codes_tool';
+    const BIBLE_REFERENCE_COMMAND = '/usr/local/bin/bib_ref_to_codes_tool';
     const BIBLE_REFERENCE_COMMAND_PARAMETERS = _BIB_REF_CMD_PARAMS_;
 
     public function build(AbstractQuery $query) {
@@ -53,7 +53,7 @@ class IxTheoQueryBuilder extends QueryBuilder
 
     private function translateToSearchString($bibleReferences) {
         $searchString = urlencode("{!bibleRangeParser}" . str_replace(":", "_", implode(',', $bibleReferences)));
-        echo "<a href='http://ptah.ub.uni-tuebingen.de:8080/solr/#/biblio/query?q=" . $searchString . "'>In Solr-Admin-Panel suchen</a>";
+        echo "<a href='http://ptah.ub.uni-tuebingen.de:8080/solr/#/biblio/query?q=" . $searchString . "&fl=bible_ranges,title'>In Solr-Admin-Panel suchen</a>";
         return "{!bibleRangeParser}" . str_replace(":", "_", implode(',', $bibleReferences));
     }
 
