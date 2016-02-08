addc0bbaf0e907b7e9ec9ccbdbfc2accd3ac3012
Oliver Obenland <oliver.obenland@uni-tuebingen.de>
2016-02-08 11:51:45 +0100

    Now bible passages can only be found by an explicit bible range search.

--- module/ixTheo/src/ixTheo/Search/Backend/Solr/IxTheoQueryBuilder.php
+++ module/ixTheo/src/ixTheo/Search/Backend/Solr/IxTheoQueryBuilder.php
@@ -2,19 +2,24 @@
 
 namespace ixTheo\Search\Backend\Solr;
 
-use VuFind\Log\Logger;
 use VuFindSearch\Backend\Solr\QueryBuilder;
 use VuFindSearch\Query\AbstractQuery;
 
 define("_BIB_REF_MAPS_PATH_", '/var/lib/tuelib/bibleRef/'); // Must end with backslash!
-define("_BIB_REF_CMD_PARAMS_", implode(' ', [_BIB_REF_MAPS_PATH_ . 'books_of_the_bible_to_code.map', _BIB_REF_MAPS_PATH_ . 'books_of_the_bible_to_canonical_form.map',  _BIB_REF_MAPS_PATH_ . 'pericopes_to_codes.map']));
+define("_BIB_REF_CMD_PARAMS_", implode(' ', [_BIB_REF_MAPS_PATH_ . 'books_of_the_bible_to_code.map', _BIB_REF_MAPS_PATH_ . 'books_of_the_bible_to_canonical_form.map', _BIB_REF_MAPS_PATH_ . 'pericopes_to_codes.map']));
 
 class IxTheoQueryBuilder extends QueryBuilder
 {
     const BIBLE_REFERENCE_COMMAND = '/usr/local/bin/bib_ref_to_codes_tool';
     const BIBLE_REFERENCE_COMMAND_PARAMETERS = _BIB_REF_CMD_PARAMS_;
 
-    public function build(AbstractQuery $query) {
+    public function build(AbstractQuery $query)
+    {
+        if ($query->getHandler() !== "BibleRangeSearch") {
+            return parent::build($query);
+        }
+        var_dump(get_class($query));
+
         // TODO: Bei Erweiterter Suche wird eine andere Query-Klasse genutzt.
         // Diese muss anders behandelt werden, da sie aus vielen Sub-Queries
         // besteht.
@@ -23,29 +28,23 @@ class IxTheoQueryBuilder extends QueryBuilder
         if (is_a($query, 'VuFindSearch\Query\QueryGroup')) {
             return parent::build($query);
         }
+
         $queryString = $query->getString();
-
-        $doBibleRangeSearch = ($query->getHandler() === "BibleRangeSearch");
-
-        if ($doBibleRangeSearch || !empty($queryString)) {
-            $newQuery =  $this->getManipulatedQueryString($query, $doBibleRangeSearch);
-            $result = parent::build($query);
-            $result->set('q', $newQuery);
-            $query->setString($queryString);
-            return $result;
-        }
-        return parent::build($query);
+        $newQuery = $this->getManipulatedQueryString($query);
+        $result = parent::build($query);
+        $result->set('q', $newQuery);
+        $query->setString($queryString);
+        return $result;
     }
 
-    private function getManipulatedQueryString(AbstractQuery $query, $doBibleRangeSearch) {
+    private function getManipulatedQueryString(AbstractQuery $query)
+    {
         $bibleReferences = $this->parseBibleReference($query);
-        if ($doBibleRangeSearch || $this->isValidBibleReference($bibleReferences)) {
-            return $this->translateToSearchString($bibleReferences);
-        }
-        return $query->getString();
+        return $this->translateToSearchString($bibleReferences);
     }
 
-    private function parseBibleReference(AbstractQuery $query) {
+    private function parseBibleReference(AbstractQuery $query)
+    {
         $searchQuery = $query->getString();
         if (!empty($searchQuery)) {
             $cmd = $this->getBibleReferenceCommand($searchQuery);
@@ -55,8 +54,9 @@ class IxTheoQueryBuilder extends QueryBuilder
         return array();
     }
 
-    private function translateToSearchString($bibleReferences) {
-        if (empty($bibleReferences )) {
+    private function translateToSearchString($bibleReferences)
+    {
+        if (empty($bibleReferences)) {
             // if no bible references were found for given query, search for a range which doesn't exist to get no result.
             $bibleReferences = ["9999999_9999999"];
         }
@@ -64,16 +64,13 @@ class IxTheoQueryBuilder extends QueryBuilder
         return $searchString;
     }
 
-    private function getBibleReferenceCommand($searchQuery) {
+    private function getBibleReferenceCommand($searchQuery)
+    {
         setlocale(LC_CTYPE, "de_DE.UTF-8");
         return implode(' ', [
-            self::BIBLE_REFERENCE_COMMAND, 
-            escapeshellarg($searchQuery), 
+            self::BIBLE_REFERENCE_COMMAND,
+            escapeshellarg($searchQuery),
             self::BIBLE_REFERENCE_COMMAND_PARAMETERS
         ]);
-    }
-
-    private function isValidBibleReference($bibleReferences) {
-        return is_array($bibleReferences) && !empty($bibleReferences);
     }
 }
