e7e01bb8b83a8393ea780a7e9285e9976be1d02d
Oliver Obenland <oliver.obenland@uni-tuebingen.de>
2016-05-10 12:25:36 +0200

    Fixing seperator of container ids an titles.

--- module/ixTheo/src/ixTheo/RecordDriver/SolrMarc.php
+++ module/ixTheo/src/ixTheo/RecordDriver/SolrMarc.php
@@ -56,10 +56,9 @@ class SolrMarc extends \VuFind\RecordDriver\SolrMarc
     public function getContainerIDsAndTitles()
     {
         $retval = array();
-
         if (isset($this->fields['container_ids_and_titles']) && !empty($this->fields['container_ids_and_titles'])) {
             foreach ($this->fields['container_ids_and_titles'] as $id_and_title) {
-                $a = explode("#31;", $id_and_title, 3); // \u00F1 gets represented by #31;.
+                $a = explode(chr(0x1F), $id_and_title, 3);
                 if (count($a) == 3) {
                     $retval[$a[0]] = array($a[1], $a[2]);
                 }
@@ -68,10 +67,10 @@ class SolrMarc extends \VuFind\RecordDriver\SolrMarc
         return $retval;
     }
 
+
     /**
      * Get all non-standardized topics
      */
-
     public function getAllNonStandardizedSubjectHeadings()
     {
        return (isset($this->fields['topic_non_standardized'])) ?
