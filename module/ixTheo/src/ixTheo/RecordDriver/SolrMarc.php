5fe8ac891f7626d2b1ad56e4d721e3c0628524f2
jriedl <johannes.riedl@uni-tuebingen.de>
2016-05-23 12:21:21 +0200

    Adjustments for adding a new electronic/non-electrnic non-electronic dimension to facets

--- module/ixTheo/src/ixTheo/RecordDriver/SolrMarc.php
+++ module/ixTheo/src/ixTheo/RecordDriver/SolrMarc.php
@@ -113,4 +113,13 @@ class SolrMarc extends \VuFind\RecordDriver\SolrMarc
     public function isSuperiorWork() {
         return isset($this->fields['is_superior_work']);
     }
+
+    /**
+     * Get the mediatype
+     */
+    public function getMediaType() 
+    {
+        return (isset($this->fields['mediatype'])) ? 
+             $this->fields['mediatype'] : '';
+    }
 }
