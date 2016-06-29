7ba75fac0aba00a39100e142215da647c0bb3193
jriedl <johannes.riedl@uni-tuebingen.de>
2016-06-29 15:33:06 +0200

    Added german as default language

--- module/VuFind/src/VuFind/Search/KeywordChainSearch/Results.php
+++ module/VuFind/src/VuFind/Search/KeywordChainSearch/Results.php
@@ -80,7 +80,7 @@ class Results extends SolrResults
 
         // Generate language extension and remove language subcode 
         $lang =  implode('', $params->get("lang"));
-        $lang_ext = $lang ? "_" . split("-", $lang)[0] : "";
+        $lang_ext = $lang ? "_" . split("-", $lang)[0] : "_de";
 
         $facet = 'key_word_chains_sorted' . $lang_ext;
         $facet_count = $facet . '-count';
