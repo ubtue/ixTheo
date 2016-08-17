fc5cf281e476ef6f6e008388cf969188bc134498
Oliver Obenland <oliver.obenland@uni-tuebingen.de>
2016-08-17 12:59:11 +0200

    Adding missing files

---
+++ components/require.config.js
@@ -0,0 +1,45 @@
+var components = {
+    "packages": [
+        {
+            "name": "bootstrap",
+            "main": "bootstrap-built.js"
+        },
+        {
+            "name": "jquery",
+            "main": "jquery-built.js"
+        },
+        {
+            "name": "jquery-ui",
+            "main": "jquery-ui-built.js"
+        },
+        {
+            "name": "modernizr",
+            "main": "modernizr-built.js"
+        }
+    ],
+    "shim": {
+        "bootstrap": {
+            "deps": [
+                "jquery"
+            ]
+        },
+        "jquery-ui": {
+            "deps": [
+                "jquery"
+            ],
+            "exports": "jQuery"
+        },
+        "modernizr": {
+            "exports": "window.Modernizr"
+        }
+    },
+    "baseUrl": "components"
+};
+if (typeof require !== "undefined" && require.config) {
+    require.config(components);
+} else {
+    var require = components;
+}
+if (typeof exports !== "undefined" && typeof module !== "undefined") {
+    module.exports = components;
+}
