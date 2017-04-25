273ab33a9da4e086adb34ab33ee223b4c3cfd2d4
ruschein <ruschein@users.noreply.github.com>
2017-04-25 16:55:04 +0200

    Update Solr.php

--- module/VuFind/src/VuFind/Autocomplete/Solr.php
+++ module/VuFind/src/VuFind/Autocomplete/Solr.php
@@ -172,7 +172,7 @@ class Solr implements AutocompleteInterface
     protected function mungeQuery($query)
     {
         // Modify the query so it makes a nice, truncated autocomplete query:
-        $forbidden = [':', '(', ')', '*', '+', '"', '–'];
+        $forbidden = [':', '(', ')', '*', '+', '"', '–' /* a hyphen, not a minus sign */];
         $query = str_replace($forbidden, " ", $query);
         if (substr($query, -1) != " ") {
             $query .= "*";
