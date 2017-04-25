ecf927a9c34901ca3c46dc25829ac1ab04cae0bd
jriedl <johannes.riedl@uni-tuebingen.de>
2017-04-25 08:24:06 +0200

    Using munge functionality for search in Autosuggest

--- module/VuFind/src/VuFind/Autocomplete/Solr.php
+++ module/VuFind/src/VuFind/Autocomplete/Solr.php
@@ -172,7 +172,7 @@ class Solr implements AutocompleteInterface
     protected function mungeQuery($query)
     {
         // Modify the query so it makes a nice, truncated autocomplete query:
-        $forbidden = [':', '(', ')', '*', '+', '"'];
+        $forbidden = [':', '(', ')', '*', '+', '"', '–'];
         $query = str_replace($forbidden, " ", $query);
         if (substr($query, -1) != " ") {
             $query .= "*";
@@ -241,7 +241,7 @@ class Solr implements AutocompleteInterface
                         $current[$field], $query, $exact
                     );
                     if ($bestMatch) {
-                        $results[] = $bestMatch;
+                        $results[] = $this->mungeQuery($bestMatch);
                         break;
                     }
                 }
