eaef0618781dec661a01995a4a49df7866832518
Mario Trojan <mario.trojan@googlemail.com>
2017-07-11 09:06:35 +0200

    moved custom citation from VuFind to ixTheo (+fixed piwik linking)

--- module/ixTheo/src/ixTheo/View/Helper/Root/Factory.php
+++ module/ixTheo/src/ixTheo/View/Helper/Root/Factory.php
@@ -5,6 +5,18 @@ use Zend\ServiceManager\ServiceManager;
 class Factory
 {
     /**
+     * Construct the Citation helper.
+     *
+     * @param ServiceManager $sm Service manager.
+     *
+     * @return Citation
+     */
+    public static function getCitation(ServiceManager $sm)
+    {
+        return new Citation($sm->getServiceLocator()->get('VuFind\DateConverter'));
+    }
+
+    /**
      * Construct the Piwik helper.
      *
      * @param ServiceManager $sm Service manager.
--- themes/ixTheoTheme/theme.config.php
+++ themes/ixTheoTheme/theme.config.php
@@ -6,7 +6,8 @@ return [
     ],
     'helpers' => [
         'factories' => [
-            'piwik' => 'VuFind\View\Helper\Root\Factory::getPiwik',
+            'citation' => 'ixTheo\View\Helper\Root\Factory::getCitation',
+            'piwik' => 'ixTheo\View\Helper\Root\Factory::getPiwik',
             'record' => 'ixTheo\View\Helper\Root\Factory::getRecord',
         ],
     ],
