2a921b0ea42ed81950ece98714e037ab85bb9a68
Mario Trojan <mario.trojan@googlemail.com>
2017-07-18 14:38:35 +0200

    improved inheritance for Browse Helper (call parent in non-customized scenario)

--- module/ixTheo/src/ixTheo/View/Helper/Root/Browse.php
+++ module/ixTheo/src/ixTheo/View/Helper/Root/Browse.php
@@ -16,26 +16,9 @@ class Browse extends \VuFind\View\Helper\Root\Browse
         $action = strToLower($action);
         $backup = strToLower($backup);
         switch($action) {
-        case 'dewey':
-            return 'dewey-hundreds';
-        case 'lcc':
-            return 'callnumber-first';
-        case 'author':
-            return 'author_facet';
-        case 'topic':
-            return 'topic_facet';
-        case 'genre':
-            return 'genre_facet';
-        case 'region':
-            return 'geographic_facet';
-        case 'era':
-            return 'era_facet';
         case 'ixtheo-classification':
             return 'ixtheo_notation_facet';
         }
-        if ($backup == null) {
-            return $action;
-        }
-        return $this->getSolrField($backup);
+        return parent::getSolrField($action, $backup);
     }
 }
