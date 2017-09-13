0154be30fa826f9e401e6c166603753d0956d789
Dr. Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2017-09-13 09:11:38 +0200

    Adjusted a path.

--- module/ixTheo/src/ixTheo/Search/Backend/Solr/QueryBuilder.php
+++ module/ixTheo/src/ixTheo/Search/Backend/Solr/QueryBuilder.php
@@ -4,7 +4,7 @@ namespace ixTheo\Search\Backend\Solr;
 
 use VuFindSearch\Query\AbstractQuery;
 
-define("_BIB_REF_MAPS_PATH_", '/var/lib/tuelib/bibleRef/'); // Must end with backslash!
+define("_BIB_REF_MAPS_PATH_", '/usr/local/var/lib/tuelib/bibleRef/'); // Must end with backslash!
 define("_BIB_REF_CMD_PARAMS_", implode(' ', [_BIB_REF_MAPS_PATH_ . 'books_of_the_bible_to_code.map', _BIB_REF_MAPS_PATH_ . 'books_of_the_bible_to_canonical_form.map', _BIB_REF_MAPS_PATH_ . 'pericopes_to_codes.map']));
 
 class QueryBuilder extends \VuFindSearch\Backend\Solr\QueryBuilder
