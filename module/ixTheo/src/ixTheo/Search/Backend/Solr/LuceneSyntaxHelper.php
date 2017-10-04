5b326c335436121eb8191f03fecf368db75c6926
ruschein <ruschein@users.noreply.github.com>
2017-10-04 10:35:35 +0200

    Update LuceneSyntaxHelper.php

--- module/ixTheo/src/ixTheo/Search/Backend/Solr/LuceneSyntaxHelper.php
+++ module/ixTheo/src/ixTheo/Search/Backend/Solr/LuceneSyntaxHelper.php
@@ -3,6 +3,9 @@
 /**
  * Lucene query syntax helper class with support for multiLangugagePArserr
  *
+ * Copyright (C) Villanova University 2010.		
+ * Copyright (C) The National Library of Finland 2016
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
  * as published by the Free Software Foundation.
